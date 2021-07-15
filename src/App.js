e6d1606425f2a972684773a1da659a5e9f8a9740
XamuelUG <samweko@gmail.com>
2021-07-15 21:04:55 +0300

    widht

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "./components/header";
 import "./app.css";
 import Main from "./components/main";
+import "line-awesome/dist/line-awesome/css/line-awesome.css";
 
 class App extends Component {
   constructor(props) {
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -34,6 +34,90 @@ class Main extends Component {
             <img src={Banner} alt="" />
           </div>
         </section>
+        <section className="vision-mission-values-ctr">
+          <div className="vision vision-mission-values">
+            <i className="las la-low-vision"></i>
+            <div>Vision</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              repellat fugit esse veritatis explicabo odit quibusdam earum,
+              unde, architecto accusantium provident, et suscipit. Quisquam est
+              illo debitis alias, deleniti labore.
+            </p>
+          </div>
+          <div className="mission vision-mission-values">
+            <i className="las la-level-up-alt"></i>
+            <div>Mission</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              repellat fugit esse veritatis explicabo odit quibusdam earum,
+              unde, architecto accusantium provident, et suscipit. Quisquam est
+              illo debitis alias, deleniti labore.
+            </p>
+          </div>
+          <div className="core-values vision-mission-values">
+            <i className="las la-glass-martini"></i>
+            <div>Core Values</div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              repellat fugit esse veritatis explicabo odit quibusdam earum,
+              unde, architecto accusantium provident, et suscipit. Quisquam est
+              illo debitis alias, deleniti labore.
+            </p>
+          </div>
+        </section>
+        <section className="why-us">
+          <div className="why-us-hdr">Why Choose Us?</div>
+          <div className="why-us-content">
+            <ul className="why-us-li">
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                officiis, recusandae dignissimos
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                officiis, recusandae dignissimos
+              </li>
+            </ul>
+            <ul className="why-us-li">
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                officiis, recusandae dignissimos
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                officiis, recusandae dignissimos
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="product-ctr">
+          <div className="product-hdr">
+            <div className="product-hdr-main">
+              SoftWare Products We are Proud To Present To You
+            </div>
+            <div className="product-hdr-caption">
+              To Solve Your ICT Challenges
+            </div>
+          </div>
+          <div className="product">
+            <div className="product-content">
+              <div className="product-content-hdr">HospitalPlus</div>
+              <div className="product-content-description">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem
+                accusamus, ab soluta maxime libero laudantium itaque incidunt
+                magni beatae? Maxime exercitationem illo illum quasi, ea sequi
+                accusantium impedit et quis.
+              </div>
+              <div className="product-content-contact">
+                <button className="services">Find Services</button>
+              </div>
+            </div>
+            <div className="product-content">
+              <img src={Banner} alt="" />
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
